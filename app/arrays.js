72e69b81d94661e54be6c938d9a586a932abbb71
juliamaksimchik <julia.maksimchik@gmail.com>
2015-01-11 19:06:03 +0300

    arrays functions

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+    	return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+    	var sum = 0;
+    	for (var i = 0; i < arr.length; i++) {
+    		sum+=arr[i];
+    	};
+    	return sum;
     },
 
     remove : function(arr, item) {
-
+    	var newArr = [];
+    	var j = 0;
+    	for (var i = 0; i < arr.length; i++) {    		
+    		if(arr[i] !== item){
+    			newArr[j] = arr[i];
+    			j++;
+    		}    		
+    	};
+    	return newArr;
     },
 
     removeWithoutCopy : function(arr, item) {
@@ -19,19 +31,23 @@ define(function() {
     },
 
     append : function(arr, item) {
-
+    	arr.push(item);
+    	return arr;
     },
 
     truncate : function(arr) {
-
+    	arr.pop();
+    	return arr;
     },
 
     prepend : function(arr, item) {
-
+    	arr.unshift(item);
+    	return arr;
     },
 
     curtail : function(arr) {
-
+    	arr.shift();
+    	return arr;
     },
 
     concat : function(arr1, arr2) {
@@ -39,11 +55,17 @@ define(function() {
     },
 
     insert : function(arr, item, index) {
-
+    	arr.splice(index, 0, item);
+    	return arr;
     },
 
     count : function(arr, item) {
-
+    	var counter = 0;
+    	for (var i = 0; i <=arr.length - 1; i++) {
+    		if(arr[i] === item)
+    			counter++;
+    	};
+    	return counter;
     },
 
     duplicates : function(arr) {
